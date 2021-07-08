574e87a9385525a89b1292a27bcb149953b405d7
filipemendes18 <filipem0512@gmail.com>
2021-07-08 20:00:00 -0300

    app completo

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:i_am_rich/shared/themes/app_images.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: Container(
-        child: Center(
-          child: Text("Hello World!"),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Center(child: Text("I Am Rich")),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: (Image.asset(AppImages.logoMain)),
         ),
       ),
     ),
