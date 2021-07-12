fae9098b319b41bf05bbd87b65b02ce5ebc571ee
filipemendes18 <filipem0512@gmail.com>
2021-07-12 12:40:26 -0300

    Desafio de Layouts

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -15,17 +15,55 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            height: 40,
-            width: 100,
-            margin: EdgeInsets.symmetric(
-                vertical: 200, horizontal: 100), // Alinha o próprio Widget
-            padding: EdgeInsets.all(10), // Alinha coisas DENTRO do Widget
-            child: Center(child: Text('Hello!')),
-            color: Colors.white,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                height: double.infinity,
+                width: 100,
+                color: Colors.red,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(color: Colors.yellow, height: 60, width: 60),
+                  Container(
+                    color: Colors.green,
+                    height: 60,
+                    width: 60,
+                  ),
+                ],
+              ),
+              Container(
+                  height: double.infinity, width: 100, color: Colors.blue),
+            ],
           ),
         ),
       ),
     );
   }
 }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         backgroundColor: Colors.teal,
+//         body: SafeArea(
+//           child: Container(
+//             height: 40,
+//             width: 100,
+//             margin: EdgeInsets.symmetric(
+//                 vertical: 200, horizontal: 100), // Alinha o próprio Widget
+//             padding: EdgeInsets.all(10), // Alinha coisas DENTRO do Widget
+//             child: Center(child: Text('Hello!')),
+//             color: Colors.white,
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
