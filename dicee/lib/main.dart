1263c4c7be81d90f1414a6ee5973051b3befe0c9
filipemendes18 <filipem0512@gmail.com>
2021-07-25 07:35:10 -0300

    App finalizado e implementado com MobX

--- dicee/lib/main.dart
+++ dicee/lib/main.dart
@@ -1,25 +1,17 @@
 import 'package:flutter/material.dart';
+import 'home/home_page.dart';
 
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.purple.shade800,
         appBar: AppBar(
           title: Center(child: Text('Dicee')),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.purple.shade700,
         ),
-        body: Dicee(),
+        body: HomeDice(),
       ),
     ),
   );
-}
-
-class Dicee extends StatelessWidget {
-  const Dicee({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
 }
