eae067f5ff3b6c89f93bc15a253aba8b2a9836f5
filipemendes18 <filipem0512@gmail.com>
2021-08-21 02:34:59 -0300

    Magic 8 Ball concluido

--- magic_8_ball/lib/main.dart
+++ magic_8_ball/lib/main.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:magic_8_ball/widgets/ball_widget.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
 }
