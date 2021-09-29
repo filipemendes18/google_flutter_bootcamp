eaa1048499609d8083629921c8ae65b061f04059
filipemendes18 <filipem0512@gmail.com>
2021-09-29 00:21:31 -0300

    Plaquinhas de som adicionadas

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -1,5 +1,4 @@
-// ignore_for_file: prefer_const_constructors
-
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,14 +6,78 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: SafeArea(
-        child: Scaffold(
-          body: Container(),
+        child: Column(
+          children: [
+            Container(
+              color: Colors.red,
+              child: TextButton(
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('audios/note1.wav');
+                  },
+                  child: Text('')),
+            ),
+            Container(
+              color: Colors.orange,
+              child: TextButton(
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('audios/note2.wav');
+                  },
+                  child: Text('')),
+            ),
+            Container(
+              color: Colors.yellow,
+              child: TextButton(
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('audios/note3.wav');
+                  },
+                  child: Text('')),
+            ),
+            Container(
+              color: Colors.green,
+              child: TextButton(
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('audios/note4.wav');
+                  },
+                  child: Text('')),
+            ),
+            Container(
+              color: Colors.teal,
+              child: TextButton(
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('audios/note5.wav');
+                  },
+                  child: Text('')),
+            ),
+            Container(
+              color: Colors.blue,
+              child: TextButton(
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('audios/note6.wav');
+                  },
+                  child: Text('')),
+            ),
+            Container(
+              color: Colors.purple,
+              child: TextButton(
+                  onPressed: () {
+                    final player = AudioCache();
+                    player.play('audios/note7.wav');
+                  },
+                  child: Text('')),
+            ),
+          ],
         ),
       ),
     );
